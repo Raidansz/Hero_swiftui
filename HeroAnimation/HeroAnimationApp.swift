a75af28a4b08a7327d5d46fa0b612e1932ade79a
raidan <raidancia0@gmail.com>
2024-07-13 21:44:05 +0200

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Raidan on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
